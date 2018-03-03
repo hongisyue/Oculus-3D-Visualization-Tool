93c30fdf7c9091e910216647e9c653a6f0e38a4f
Unknown <scott_laskowski@live.com>
2018-03-03 13:56:03 -0600

    Hong Test Add
    Checking Hong's test

--- functions/test/e2etesting.js
+++ functions/test/e2etesting.js
@@ -1,161 +1,202 @@
-// /**
-//  * Created by homeyxue on 2018-02-03.
-//  */
-// describe("Inner Suite 1", function(){
-//     browser.ignoreSynchronization = true;   //if not written by angluar
-//     browser.get("https://3dvisualizationtool.ml");
+/**
+ * Created by homeyxue on 2018-02-03.
+ */
 
-//     /**
-//      * browser set up and selenium web-driver buid
-//      */
-//     var webdriver = require('selenium-webdriver')
-//     var chrome = require('selenium-webdriver/chrome');
-//     var firefox = require('selenium-webdriver/firefox');
+//selenium web driver require for all test suite
+var webdriver = require('selenium-webdriver');
+  //  { describe, it, before, after } = require('selenium-webdriver/testing');
 
-//     var profile = new firefox.Profile( '/Users/homeyxue/Library/Application Support/Firefox/Profiles/8htp4yu0.dev-edition-default');
-//     var firefoxOptions = new firefoxOptions().setProfile(profile);
+        By = webdriver.By,
+        until = webdriver.until,
+        assert = require('assert');
+/**
+ * Using Firefox for testing, we need to install geckodriver
+ * Using chrom for testing, we need to install chromedriver
+ */
+//var firefox = require('selenium-webdriver/firefox');
 
-//     var profile1 = new chrome.Profile('/Users/homeyxue/Library/Application Support/Google/Chrome/Default');
-//     var chromeOptions = new chromeOptions().setProfile(profile1);
+//var profile = new firefox.Profile( '/Users/homeyxue/Library/Application Support/Firefox/Profiles/8htp4yu0.dev-edition-default');
+//var firefoxOptions = new firefoxOptions().setProfile(profile);
+describe('home page test Suite', function(done){
+  this.timeout(50000);
+    /**
+     * web driver for test case
+     */
+    before(() =>{
+        driver = new webdriver.Builder()
+            .forBrowser('firefox')
+            .build();
+        driver.get("https://3dvisualizationtool.ml");
 
-//     var driver = new webdriver.Builder()
-//         .forBrowser('firefox')
-//         .setFirefoxOptions(firefoxOptions)
-//         .setChromeOptions(chromeOptions)
-//         .build();
+        driver.findElement(By.css('input')).sendKeys('oculus3dvisualizationtool@gmail.com');
+        driver.findElement(By.id('identifierNext')).click();
 
-//     /**
-//      * test suite setup
-//      */
-//     before(function(){
+        driver.wait(until.elementLocated(By.id('passwordNext')), 5000);
+        driver.findElement(By.css('input')).sendKeys('osgood371');
+        driver.findElement(By.id('passwordNext')).click();
+    });
 
-//         driver.get("https://3dvisualizationtool.ml");
+    /**
+     * close web driver after test case
+     */
+    after(() =>{
+        driver.quit();
+    });
 
-//         // a promise is returned while ‘click’ action is registered in ‘driver’ object
-//         //return driver.findElement(webdriver.By.id(loadLocal)).click();  /** id=loadLocal have NOT been added into html yet*/
-//     });
+    it("title should print", () =>{
 
-//     /**
-//      * web-driver close after test suite finish
-//      */
-//     after(function(){
+        driver.getTitle().then(function ( title ) {
+            console.log(title);
 
-//         return driver.quit();
+            assert(title === "3D Visualization Tool");
+        });
+    });
 
-//     });
+    it("home should be active", () => {
 
-//     beforeEach(function(){
+      var home = driver.findElement(By.name('Home'));
+      home.getAttribute('className').then(function(field){
+        assert(field === 'active');
+      });
+    })
 
-//         // do something before test case execution
-//         // no matter if there are failed cases
+    it("click Home should work", () =>{
 
-//     });
+      var home = driver.findElement(By.name('Home'));
+      home.click();
+      driver.wait(until.elementLocated(By.className('masthead-brand')), 5000);     //should stay in the same page
+      driver.getCurrentUrl().then(function( url ){
+          assert(url === "https://3dvisualizationtool.ml");
+      });
+    });
 
-//     /**
-//      * test suite teardown repeat(not use yet)
-//      */
-//     afterEach(function(){
+    it("click Features should work", () =>{
 
-//         // do something after test case execution is finished
-//         // no matter if there are failed cases
+      var feature = driver.findElement(By.name('Features'));     /** Dashboard button should click*/
+      feature.click();
+      driver.wait(until.elementLocated(By.className('inner cover')), 5000);     //'inner cover' is a id of a new element, therefore should work'
+      driver.getCurrentUrl().then(function( url ){
+          assert(url === "https://3dvisualizationtool.ml/features");
+      });
+    });
 
-//     });
+    it("click About Us should work", () =>{
 
-//     it("Timeout", function(){
+      var about = driver.findElement(By.name('About Us'));     /** Dashboard button should click*/
+      about.click();
+      driver.wait(until.elementLocated(By.className('inner cover')), 5000);     //'inner cover' is a id of a new element, therefore should work'
+      driver.getCurrentUrl().then(function( url ){
+          assert(url === "https://3dvisualizationtool.ml/about");
+      });
+    });
 
-//         var promise;
-//         promise = new Promise(function(resolve, reject){
-//             setTimeout(function(){
+    it("click Dashboard(nav bar) should work", () =>{
 
-//                 console.log("Timeout");
-//                 resolve();
+      var about = driver.findElement(By.name('Dashboard'));     /** Dashboard button should click*/
+      about.click();
+      driver.wait(until.elementLocated(By.id('contentBox')), 5000);     //'contentBox' is a id of a new element, therefore should work'
+      driver.getCurrentUrl().then(function( url ){
+          assert(url === "https://3dvisualizationtool.ml/dashboard");
+      });
+    });
 
-//             }, 3000);
+    it("click dashboard button should work", () =>{
 
-//         });
-//         // mocha will wait for the promise to be resolved before exiting
-//         return promise;
-//     });
+        var dashboard = driver.findElement(By.className('btn btn-lg btn-default'));     /** Dashboard button should click*/
+        dashboard.click();
+        driver.wait(until.elementLocated(By.id('contentBox')), 5000);     //'contentBox' is a id of a new element, therefore should work'
+        driver.getCurrentUrl().then(function( url ){
+            assert(url === "https://3dvisualizationtool.ml/dashboard");
+        });
+    });
+});
 
-//     it("Title check", function(){
+describe("Dashboard page test Suite", function(done){
+  this.timeout(50000);
+    /**
+     * web driver for test case
+     */
+    before(function(){
+        driver = new webdriver.Builder()
+            .forBrowser('firefox')
+            .build();
+        driver.get("https://3dvisualizationtool.ml/dashboard");
+    });
 
-//         var my_title = "3D Visualization Tool";
-//         driver.getTitle().then(function ( title ) {
-//             expect(title).equals( my_title );
-//             console.log(title)      // assertions: "3D Visualization Tool" should be printed
-//         });
-//     });
+    /**
+     * close web driver after test case
+     */
+    after(function(){
+        driver.quit();
+    });
 
-//     it("home button should be active", function(){
+/**
+ * Command out for now
+ * because google switch account is not working yet
+ */
 
-//         var my_classType = 'active';
-//         var my_href = "/";
+/**
+    it("click google button should work", () =>{
 
-//         var button = element.all(by.className(my_classType));
-//         expect(button.href).toEqual(my_href);
-//     });
+        var google = driver.findElement(By.id('connecteddq7u6owam3f3'));
+        google.click();
+        //TODO: test switch account function
+    });
+    */
 
-//     it("home page body display test", function() {
+    it("dashboard should be active", () => {
 
-//         var body_1 = element.all(by.className('site-wapper'));
-//         var body_2 = element.all(by.className('site-wapper-inner'));
-//         var body_3 = element.all(by.className('container'));
+      var dash = driver.findElement(By.name('Dashboard'));
+      dash.getAttribute('className').then(function(field){
+        assert(field === 'active');
+      });
+    })
 
-//         expect(body_1.isDisplayed()).toEqual([true]);
-//         expect(body_2.isDisplayed()).toEqual([true]);
-//         expect(body_3.isDisplayed()).toEqual([true]);
-//     })
+    it("click VRworld should work", () => {
 
-//     it("home page title & nav display check", function() {
+        var VRw0 = driver.findElement(By.id('-L6UcY0EKaTU24sRbEq3'));
+        var VRw1 = driver.findElement(By.id('-L6UdSwZZBWXNHBhp9cf'));
+        var VRw2 = driver.findElement(By.id('-L6Ud_fayHJ9kHcmOwRf'));
 
-//         var top = element.all(by.className('container inner'));
-//         var title = element.all(by.className('masthead-brand'));
-//         var nav = element.all(by.className('nav masthead-nav'));
+        VRw0.click();
+        driver.wait(until.elementLocated(By.name('VR Visualization Tool')), 5000);     //'VR Visualization Tool' is a new element, therefore should work'
+        driver.getCurrentUrl().then(function( url ){
+            assert(url === "https://3dvisualizationtool.ml/VRWorld");
+        });
+    })
 
-//         expect(top.isDisplayed()).toEqual([true]);
-//         expect(title.isDisplayed()).toEqual([true]);
-//         expect(nav.isDisplayed()).toEqual([true]);
-//     })
+    it("VRworld should work without VR", () => {
 
-//     it("load local page check", function() {
+        var VRw1 = driver.findElement(By.id('-L6UdSwZZBWXNHBhp9cf'));
+        VRw1.click();
+        driver.wait(until.elementLocated(By.name('VR Visualization Tool')), 5000);     //'VR Visualization Tool' is a new element, therefore should work'
 
-//         // a promise is returned while ‘click’ action is registered in ‘driver’ object
-//         driver.findElement(webdriver.By.id(loadLocal)).click();  /** id=loadLocal have NOT been added into html yet*/
+        var noVR = driver.findElement(By.name('Try it without a headset'));
+        noVR.click();
+        driver.wait(until.elementLocated(By.name('EXIT VR')), 5000);
 
-//         var my_classType = 'active';
-//         var my_href = "/localLoad";
+        var display = driver.findElement(By.id('ui'));
+        display.getAttribute('style').then(function(field){
+          assert(field === 'display: none;');
+        });
+    })
 
-//         var button = element.all(by.className(my_classType));
-//         expect(button.href).toEqual(my_href);   //oadLocal button should be in class 'active'
-//         /**
-//          * TODO: click load local(href = /localLoad); get load file; check data correctness
-//          */
-//     })
+    it("should load local file", function() {
 
+        var button = driver.findElement(By.className('btn btn-info btn-lg'));
+        button.click();
+        driver.wait(until.elementLocated(By.id('myWizard')), 3000);
 
+        var button2 = driver.findElement(By.name('Local'));
+        button2.click();
+        driver.wait(until.elementLocated(By.id('step2')), 3000);
 
-// });
+        var FILE_PATH = '/Users/homeyxue/Oculus-3D-Visualization-Tool/dev_helpers/cities.csv';
 
+        driver.findElement(By.className('form-control')).sendKeys(FILE_PATH);
+        var button3 = driver.findElement(By.name('Load!'));
+        button3.click();
 
+    })
 
-
-// /**
-// module.exports = {
-//     entry: './lib/e2etesting.js',
-//     output: {
-//         filename: 'bundle.js',
-//         path: path.resolve(__dirname, 'dist')
-//     },
-
-//     const {By} = require('selenium-webdriver'),
-//     it('should work', async function () {
-//         await driver.get('http://localhost:8080')
-//         //...
-
-//         await retry(async () => {
-//             const displayElement = await driver.findElement(By.css('.display'))
-//             const displayText = await displayElement.getText()
-
-//             expect(displayText).to.equal('0')
-//         })
-// */
+});
